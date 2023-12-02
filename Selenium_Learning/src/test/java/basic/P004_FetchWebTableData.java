9794b1ae8ae2d783d1271abcba7086ad5aaf4d73
jay-java <jayprajapati.tops@gmail.com>
2023-12-02 12:37:30 +0530

    webatble new

--- Selenium_Learning/src/test/java/basic/P004_FetchWebTableData.java
+++ Selenium_Learning/src/test/java/basic/P004_FetchWebTableData.java
@@ -12,14 +12,24 @@ public class P004_FetchWebTableData {
 	public static void main(String[] args) {
 		String url = "https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html";
 		WebDriver driver = DriverConnection.getDriver(url);
-		List<WebElement> list = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
-		System.out.println(list.size());
-		for(int i=1;i<list.size();i++) {
-			List<WebElement> thead = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr["+i+"]/th"));
-			System.out.println(thead.size());
-			for(int j = 1;j<thead.size();j++) {
-				WebElement th_data = driver.findElement(By.xpath("//table[@id='customers']/tbody/tr["+i+"]/th/["+j+"]/span"));
-				System.out.println(th_data.getText());
+		List<WebElement> thead = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr[1]/th/span"));
+		System.out.println(thead.size());
+		for(WebElement e:thead) {
+			String head_text = e.getText();
+			System.out.print(head_text+"  ");
+		}
+		System.out.println();
+		List<WebElement> table_row = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
+		System.out.println(table_row.size());
+		for(int r= 2;r<=table_row.size();r++) {
+			List<WebElement> table_data = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr["+r+"]/td"));
+			System.out.println(table_data.size());
+			for(int d=1;d<=table_data.size();d++) {
+				List<WebElement> table_data_text = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr["+r+"]/td["+d+"]/span"));
+				for(WebElement s:table_data_text) {
+					String span_text = s.getText();
+					System.out.println(span_text);
+				}
 			}
 		}
 	}
